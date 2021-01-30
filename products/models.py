490d6df5feaee1b825b1cf85fd01527959957954
Hossam_Cj <hossamcj@gmail.com>
2021-01-31 00:38:25 +0200

    Renew the Product Model

--- products/models.py
+++ products/models.py
@@ -5,6 +5,9 @@ from django.urls import reverse
 class Product(models.Model):
     title = models.CharField(max_length=124)
     description = models.TextField(max_length=500, blank=True, null=True)
+    brand = models.CharField(max_length=124)
+    price = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
         return reverse('product_details', args=(self.id,))
