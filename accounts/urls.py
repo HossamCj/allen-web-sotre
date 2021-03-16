1571134b9e7642a5630dc1cb249eb94df6208b76
Hossam_Cj <hossamcj@gmail.com>
2021-03-16 17:49:02 +0200

    Created Cart app with auto cart creating feature for new users

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     path('activate/<uid>/<token>/', activate_email, name='activate_email'),
 
     path('password-reset', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('password-reset/confirm/<uidb64>/<token>',
          auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('password-reset/complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 ]
