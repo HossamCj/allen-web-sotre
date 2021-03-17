c1eff3d05565b37898c756cde1982ba29225f51e
Hossam_Cj <hossamcj@gmail.com>
2021-03-17 08:38:10 +0200

    Update requirements.txt file

--- store/prod_settings.py
+++ store/prod_settings.py
@@ -9,4 +9,4 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['allen-web-store.herokuapp.com']
 
-DATABASES = dj_database_url.config(os.environ.get('DATABASE_URL'))
+DATABASES = dj_database_url.config(default=os.environ.get('DATABASE_URL'))
